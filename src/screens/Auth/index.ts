485093177d54ed072496eb032ce7cac89178b288
Tani <artanmuzhaqi@gmail.com>
2025-08-22 06:20:09 -0400

    Fix casing: rename auth to Auth

--- src/screens/Auth/index.ts
+++
@@ -1,6 +0,0 @@
-export {LoginScreen} from './LoginScreen';
-export {SignUpScreen} from './SignUpScreen';
-export {ForgotPasswordScreen} from './ForgotPasswordScreen';
-export {RememberLoginInfoScreen} from './RememberLoginInfo';
-export {CodeVerificationScreen} from './CodeVerificationScreen';
-export {LandingAuthScreen} from './LandingAuthScreen';
--- src/screens/OnBoarding/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './CreateHomeScreen';
-export * from './CreateShoppingListScreen';
-export * from './SelectPantryItems';
-export * from './InviteMemberScreen';
-export * from './OnboardingCompleteScreen';
