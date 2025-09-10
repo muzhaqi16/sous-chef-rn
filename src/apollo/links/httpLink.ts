26e76d997cd90c53a5fd10dee056d6fbb0bbb8a5
Tani <artanmuzhaqi@gmail.com>
2025-09-09 20:18:23 -0400

    add url env

--- src/apollo/links/httpLink.ts
+++ src/apollo/links/httpLink.ts
@@ -1,5 +1,5 @@
 import {createHttpLink} from '@apollo/client';
 
 export const httpLink = createHttpLink({
-  uri:  process.env.API_KEY
+  uri:  process.env.API_URL || 'http://localhost:4000/graphql',
 });
