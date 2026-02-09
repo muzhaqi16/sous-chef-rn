1ad634ddd3a91e5d4a339693d40048cb45225d4f
Tani <artan@muzhaqi.com>
2026-02-09 14:45:55 -0500

    add tree source mapping

--- metro.config.js
+++ metro.config.js
@@ -11,7 +11,16 @@ const defaultConfig = getDefaultConfig(__dirname);
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 
 module.exports = wrapWithReanimatedMetroConfig(
   mergeConfig(defaultConfig, config),
