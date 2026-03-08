6915b5ce6329d3857c9769a32243bbd1c25c6894
Tani <artan@muzhaqi.com>
2026-03-07 19:12:20 -0500

    add xcode cloud fix

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 set -e
 
-cd "$CI_PRIMARY_REPOSITORY_PATH"
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+# Install Node 24 LTS
+brew install node@24
+brew link node@24 --force
+
+# Install CocoaPods
+brew install cocoapods
+
+# Install JS dependencies
+cd /Volumes/workspace/repository
 npm install
+
+# Install iOS pod dependencies
+cd ios
+pod install
